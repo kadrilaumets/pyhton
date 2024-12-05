37e1ae4a7238285c1fe5d6f8d86a601df3124299
kadrilaumets <kadrilaumets@hkhk.edu.ee>
2024-12-05 14:50:43 +0200

    süda

--- 01.py
+++ 01.py
@@ -1,7 +1,8 @@
 # 01.ülesanne
 # Kadri 05.12.2024
 import turtle
-turtle.speed(0)
+#kolmnurk
+turtle.speed(6) #reguleeri 1-9
 turtle.penup()
 turtle.goto(-500,200) #liigutab asukohta
 turtle.pendown()
@@ -12,6 +13,16 @@ turtle.left(120)
 turtle.forward(200)
 turtle.left(120)
 
+# süda
+
+turtle.penup()
+turtle.goto(-200,200) # liigutab asukohta
+turtle.pendown()
+turtle.forward(100)
+turtle.circle(50,180)
+turtle.right(90)
+turtle.circle(50,180)
+turtle.fd(100)
 
 # lõpetab, ennetab kokku jooksmise
 turtle.done()
