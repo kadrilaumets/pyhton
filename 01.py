0fd8afb3db8705b3de2437cfc245e5ad8d9aaa17
kadrilaumets <kadrilaumets@hkhk.edu.ee>
2024-12-05 14:37:44 +0200

    kolmnurk

---
+++ 01.py
@@ -0,0 +1,17 @@
+# 01.ülesanne
+# Kadri 05.12.2024
+import turtle
+turtle.speed(0)
+turtle.penup()
+turtle.goto(-500,200) #liigutab asukohta
+turtle.pendown()
+turtle.forward(200)# liigub edasi,pikslites
+turtle.left(120)
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(200)
+turtle.left(120)
+
+
+# lõpetab, ennetab kokku jooksmise
+turtle.done()
