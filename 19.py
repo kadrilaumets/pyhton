21e03cf19badc6e36c0df9954944f2b5cbbcca2f
kadrilaumets <kadrilaumets@hkhk.edu.ee>
2025-02-14 09:38:04 +0200

    meow

---
+++ 19.py
@@ -0,0 +1,32 @@
+#14.02
+#kadri laumets
+#harjutus 19
+
+import json
+
+klass12opilased = []
+kokku12klass = 0
+huvialad = []
+
+# Faili avamine ja JSON-i laadimine
+with open('haridustulemused.json', 'r', encoding='utf-8') as file:
+    students = json.load(file)
+    for student in students:
+        #print(student["klass"])
+        if student["klass"] =="12":
+            klass12opilased.append(student["nimi"])
+            kokku12klass+=1
+            for tegevus in student["tegevused"]:
+                if tegevus not in huvialad:
+                    huvialad.append(tegevus)
+             #hinneteleht       
+            print("-------HINNETELEHT--------")
+            print(student["nimi"])
+            d = (student["hinded"])
+            for k, v in d.items():
+                print(k,v)
+            print("------------------------")
+# print(klass12opilased)
+# print(kokku12klass, "õpilast")
+# for i in huvialad:
+#     print(i)
